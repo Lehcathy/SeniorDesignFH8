76360000be93a5114a4bec09297d49e1230bbd7c
jwallace22 <jeffreywallace@utexas.edu>
2021-11-18 23:21:54 -0600

    Added erasing and saving functions for sketching

--- SketchYourOwnGAN/ContentView.swift
+++ SketchYourOwnGAN/ContentView.swift
@@ -11,17 +11,37 @@ import PencilKit
 struct ContentView: View {
     @State var sketching = true
     @State var canvas = PKCanvasView()
-    
+    @State var erasing = false
     var body: some View {
         if sketching {
             VStack{
-                Text("Sketching")
-                DrawingView(canvas: $canvas).navigationTitle("Sketching").navigationBarTitleDisplayMode(.inline)
+                DrawingView(canvas: $canvas, erasing: $erasing).navigationTitle("Sketching").navigationBarTitleDisplayMode(.inline)
                 HStack {
-                    Button("Save Sketch", action: {sketching=false}).buttonStyle(.bordered).background().colorMultiply(.blue).foregroundColor(.black)
+                    Spacer()
+                    Button(action: {
+                        let image = canvas.drawing.image(from: canvas.drawing.bounds, scale: 1)
+                        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                    }, label: {
+                        Image(systemName: "square.and.arrow.down.fill").font(.title)
+                    })
+                    Spacer()
                     Button("To GAN", action: {sketching=false}).buttonStyle(.bordered).background().colorMultiply(.blue).foregroundColor(.black)
+                    Spacer()
+                    Button(action: {
+                        erasing = false
+                    }, label: {
+                        Image(systemName: "pencil").font(.title)
+                    })
+                    Spacer()
+                    Button(action: {
+                        erasing = true
+                    }, label: {
+                        Image(systemName: "pencil.slash").font(.title)
+                    })
+                    Spacer()
                 }
                 Spacer()
+            
             }
         } else {
             VStack {
@@ -46,12 +66,21 @@ func RunGan() {
 
 struct DrawingView: UIViewRepresentable {
     @Binding var canvas: PKCanvasView
+    @Binding var erasing : Bool
+    let pen = PKInkingTool(.pen, color: .black)
+    let eraser = PKEraserTool(.bitmap)
+    
     func makeUIView(context: Context) -> PKCanvasView {
         canvas.drawingPolicy = .anyInput
+        if erasing {
+            canvas.tool = eraser
+        } else {
+            canvas.tool = pen
+        }
         return canvas
     }
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        
+        uiView.tool = erasing ? eraser : pen
     }
 }
 struct ContentView_Previews: PreviewProvider {
