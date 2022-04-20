c0b969395115e4f284ccb204f897685853cbdfd1
jwallace22 <jeffreywallace@utexas.edu>
2022-04-19 22:45:35 -0500

    Revert "resetting"
    This reverts commit f60afc657a1a9300ced7b44ed4faced17ba08e24.

--- SketchYourOwnGAN/GAN.swift
+++ SketchYourOwnGAN/GAN.swift
@@ -13,7 +13,6 @@ class GAN{
     let context = CIContext()
     var outputImage: UIImage?
     func coreMLCompletionHandler(request:VNRequest?,error:Error?){
-<<<<<<< HEAD
         let result = request!.results!.first as! VNCoreMLFeatureValueObservation//VNPixelBufferObservation
         let multiArray = result.featureValue.multiArrayValue
         print(multiArray)
@@ -23,17 +22,10 @@ class GAN{
         //let ciimage = CIImage(cvPixelBuffer: result.pixelBuffer)
         //let cgImage = self.context.createCGImage(ciimage, from: ciimage.extent)
         //self.outputImage = UIImage(cgImage: cgImage!)
-=======
-        let result = request!.results!.first as! VNPixelBufferObservation
-        let ciimage = CIImage(cvPixelBuffer: result.pixelBuffer)
-        let cgImage = self.context.createCGImage(ciimage, from: ciimage.extent)
-        self.outputImage = UIImage(cgImage: cgImage!)
->>>>>>> parent of 01fa613... Testing PhotoSketch
     }
     func RunGan(sketch: CGImage) {
         lazy var coreMLRequest:VNCoreMLRequest = {
             let config = MLModelConfiguration()
-<<<<<<< HEAD
            let model = try! VNCoreMLModel(for: photosketch2(configuration: config).model)
            let request = VNCoreMLRequest(model: model, completionHandler: self.coreMLCompletionHandler)
             return request
@@ -41,14 +33,6 @@ class GAN{
         let pixelBuffer = sketch.pixelBuffer(width: 256, height: 256, orientation: .up)!
         let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
         //let handler = VNImageRequestHandler(ciImage: sketch,options: [:])
-=======
-           let model = try! VNCoreMLModel(for: ugatit(configuration: config).model)
-           let request = VNCoreMLRequest(model: model, completionHandler: self.coreMLCompletionHandler)
-            return request
-           }()
-
-        let handler = VNImageRequestHandler(ciImage: sketch,options: [:])
->>>>>>> parent of 01fa613... Testing PhotoSketch
         try? handler.perform([coreMLRequest])
     }
 }
